bdbe9dcfd4f88876957f017ec9212327ecdf5d9c
Illyanda-D <andregervasio97dev@gmail.com>
2025-08-02 10:00:57 -0300

    changed theme to macchiato default, and transparent bg. Added lualine

--- lua/theme/colorscheme.lua
+++ lua/theme/colorscheme.lua
@@ -1,6 +1,22 @@
 -- Colorscheme
 vim.pack.add({
-	{ src = 'https://github.com/catppuccin/nvim' }
+	{ src = 'https://github.com/catppuccin/nvim' },
+	{ src = 'https://github.com/nvim-lualine/lualine.nvim' },
 })
-vim.cmd('colorscheme catppuccin-macchiato')
-vim.cmd(':hi statusline guibg=NONE')
+
+require('catppuccin').setup({
+	flavour = 'macchiato',
+	transparent_background = true,
+	styles = {
+		booleans = { 'bold' }
+	},
+	auto_integrations = true,
+})
+
+require('lualine').setup({
+	options = {
+		theme = 'palenight',
+	},
+})
+
+vim.cmd.colorscheme 'catppuccin'
